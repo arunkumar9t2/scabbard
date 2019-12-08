a7893a6cb1eb63b8c1c0ad445561712c05c21c1a
Arunkumar <arunk.beece@gmail.com>
2019-12-09 01:14:27 +0800

    Render Scope Colors in graph

--- scabbard-processor/src/main/java/dev/arunkumar/scabbard/plugin/GraphVizBindingGraphProcessor.kt
+++ scabbard-processor/src/main/java/dev/arunkumar/scabbard/plugin/GraphVizBindingGraphProcessor.kt
@@ -7,6 +7,7 @@ import dev.arunkumar.graphviz.dsl.*
 import dev.arunkumar.scabbard.plugin.util.component1
 import dev.arunkumar.scabbard.plugin.util.component2
 import guru.nidi.graphviz.attribute.Color
+import guru.nidi.graphviz.attribute.Color.*
 import guru.nidi.graphviz.attribute.Rank
 import guru.nidi.graphviz.attribute.Shape
 import guru.nidi.graphviz.attribute.Style
@@ -27,6 +28,22 @@ constructor(
     private val filer: Filer
 ) : BindingGraphProcessor {
 
+    private val scopeColors by lazy {
+        listOf(
+            AQUAMARINE,
+            BLUE,
+            CYAN,
+            MAGENTA,
+            TOMATO,
+            YELLOW,
+            DEEPPINK,
+            GOLD,
+            CRIMSON,
+            CHOCOLATE1,
+            CADETBLUE1
+        )
+    }
+
     override fun process() {
         val network = bindingGraph.network()
         val nodes = network.nodes()
@@ -35,10 +52,20 @@ constructor(
         val nodeIds = HashMap<BindingGraph.Node, UUID>()
         fun BindingGraph.Node.id() = nodeIds.computeIfAbsent(this) { UUID.randomUUID() }.toString()
 
-        nodes
-            .asSequence()
+        val scopeColorCache = HashMap<String, Color>().apply {
+            put("", TURQUOISE)
+        }
+
+        fun Binding.color() = scopeColorCache.computeIfAbsent(scopeName() ?: "") {
+            scopeColors.random()
+        }
+
+        nodes.asSequence()
             .groupBy { it.componentPath() }
             .forEach { (component, nodes) ->
+                // Cache all added nodes for edge linking later
+                val nodesCache = HashMap<String, MutableNode>()
+
                 val currentComponent = component.currentComponent()
                 val componentName = ClassName.get(currentComponent)
 
@@ -48,7 +75,7 @@ constructor(
                     componentName.simpleNames().joinToString("_").plus(".png")
                 )
 
-                val builder = mutGraph(name = currentComponent.toString(), directed = true) {
+                val rootGraph = mutGraph(name = currentComponent.toString(), directed = true) {
                     graphAttr {
                         add(Rank.dir(Rank.RankDir.LEFT_TO_RIGHT))
                         add("labeljust", "l")
@@ -58,17 +85,16 @@ constructor(
                     nodeAttr {
                         add(Shape.RECTANGLE)
                         add(Style.FILLED)
-                        add(Color.TURQUOISE)
+                        add(TURQUOISE)
                     }
 
-                    // Cache all added nodes for edge linking later
-                    val nodesCache = HashMap<String, MutableNode>()
-                    // Render all nodes
+                    // Add all valid nodes
                     nodes.forEach { node ->
                         when (node) {
                             is Binding -> {
                                 mutableNode(node.id()) {
-                                    add("label" to node.key().toString())
+                                    add("label" to node.label())
+                                    add(node.color())
                                     nodesCache[node.id()] = this
                                 }
                             }
@@ -90,9 +116,26 @@ constructor(
                     }
                 }
 
-                Graphviz.fromGraph(builder)
+                Graphviz.fromGraph(rootGraph)
                     .render(Format.PNG)
                     .toOutputStream(outputFile.openOutputStream())
             }
+    }
+
+    protected fun Binding.scopeName() = if (scope().isPresent) {
+        "@" + scope().get().scopeAnnotationElement().simpleName.toString()
+    } else {
+        null
+    }
+
+    protected fun BindingGraph.Node.label(): String = when (this) {
+        is Binding -> {
+            val scopeName = scopeName()
+            when {
+                scopeName != null -> "$scopeName\\n${key()}"
+                else -> key().toString()
+            }
+        }
+        else -> componentPath().toString()
     }
 }
