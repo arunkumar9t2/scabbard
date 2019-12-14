c248dfbcac41404a10b27dcb5f87183d8dacc3bc
Arunkumar <arunk.beece@gmail.com>
2019-12-14 18:00:24 +0800

    Scaffold IntelliJ plugin for scabbard

---
+++ scabbard-idea-plugin/src/main/java/dev/arunkumar/scabbard/intellij/JavaComponentToDaggerGraphLineMarker.kt
@@ -0,0 +1,16 @@
+package dev.arunkumar.scabbard.intellij
+
+import com.intellij.codeInsight.daemon.LineMarkerInfo
+import com.intellij.codeInsight.daemon.LineMarkerProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiMethod
+
+class JavaComponentToDaggerGraphLineMarker : LineMarkerProvider {
+
+  override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
+    if (element is PsiMethod) {
+
+    }
+    return null
+  }
+}
---
+++ scabbard-idea-plugin/src/main/java/dev/arunkumar/scabbard/intellij/utill/constants.kt
@@ -0,0 +1,4 @@
+package dev.arunkumar.scabbard.intellij.utill
+
+const val DAGGER_COMPONENT = "dagger.Component"
+const val DAGGER_SUBCOMPONENT = "dagger.Subcomponent"
