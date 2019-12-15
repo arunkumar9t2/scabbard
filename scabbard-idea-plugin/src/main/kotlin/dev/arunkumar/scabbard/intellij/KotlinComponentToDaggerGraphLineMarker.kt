3a1a448f72d9a5e1b18b45dafe93d91651f05375
Arunkumar <arunk.beece@gmail.com>
2019-12-15 18:57:35 +0800

    Revert to using global file name scope as module level scope will not work when files are generated only in the module where scabbard processor is applied

--- scabbard-idea-plugin/src/main/kotlin/dev/arunkumar/scabbard/intellij/KotlinComponentToDaggerGraphLineMarker.kt
+++ scabbard-idea-plugin/src/main/kotlin/dev/arunkumar/scabbard/intellij/KotlinComponentToDaggerGraphLineMarker.kt
@@ -9,14 +9,11 @@ import com.intellij.openapi.util.IconLoader
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.search.FilenameIndex
-import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.GlobalSearchScope.allScope
-import com.intellij.psi.search.GlobalSearchScope.moduleWithDependenciesAndLibrariesScope
 import com.intellij.util.Function
 import dev.arunkumar.scabbard.intellij.utill.DAGGER_COMPONENT
 import dev.arunkumar.scabbard.intellij.utill.DAGGER_SUBCOMPONENT
 import org.jetbrains.kotlin.idea.util.findAnnotation
-import org.jetbrains.kotlin.idea.util.module
 import org.jetbrains.kotlin.lexer.KtKeywordToken
 import org.jetbrains.kotlin.lexer.KtTokens.*
 import org.jetbrains.kotlin.name.FqName
@@ -39,10 +36,13 @@ class KotlinComponentToDaggerGraphLineMarker : LineMarkerProvider {
               val directoryShouldEndWith =
                 packageName.replace(".", separator) + separator + ktClass.name
 
-              val scope: GlobalSearchScope = when {
+              /*val scope: GlobalSearchScope = when {
                 element.module != null -> moduleWithDependenciesAndLibrariesScope(element.module!!)
                 else -> allScope(project)
-              }
+              }*/
+
+              // TODO(arun) Need to optimize this
+              val scope = allScope(element.project)
 
               FilenameIndex.getFilesByName(project, fileNameToFind, scope)
                 .filter { it.containingDirectory.toString().endsWith(directoryShouldEndWith) }
