bc38b4c05fa45d96278e742cba868d1515480e3b
Arunkumar <arunk.beece@gmail.com>
2019-12-19 06:03:40 +0800

    Forward certain extension properties from Scabbard gradle extensions to kapt arguments

--- scabbard-gradle-plugin/src/main/kotlin/dev/arunkumar/scabbard/gradle/ScabbardGradlePlugin.kt
+++ scabbard-gradle-plugin/src/main/kotlin/dev/arunkumar/scabbard/gradle/ScabbardGradlePlugin.kt
@@ -3,6 +3,8 @@ package dev.arunkumar.scabbard.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.findByType
+import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 class ScabbardGradlePlugin : Plugin<Project> {
 
@@ -10,6 +12,13 @@ class ScabbardGradlePlugin : Plugin<Project> {
     val scabbardExtension = project.extensions.create<ScabbardExtension>(SCABBARD)
     if (scabbardExtension.enabled) {
       project.dependencies.add("kapt", "dev.arunkumar:scabbard-processor:0.1.0")
+    }
+
+    project.extensions.findByType<KaptExtension>()?.apply {
+      arguments {
+        arg("$SCABBARD.singleGraph", scabbardExtension.singleGraph)
+        arg("$SCABBARD.failOnError", scabbardExtension.failOnError)
+      }
     }
   }
 }
