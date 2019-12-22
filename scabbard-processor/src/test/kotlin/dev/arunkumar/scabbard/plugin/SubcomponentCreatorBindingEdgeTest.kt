e2c75e61b568527efb1b2b7995143807aeb47110
Arunkumar <arunk.beece@gmail.com>
2019-12-22 14:07:42 +0800

    SubComponentCreatorBinding tests

---
+++ scabbard-processor/src/test/kotlin/dev/arunkumar/scabbard/plugin/SubcomponentCreatorBindingEdgeTest.kt
@@ -0,0 +1,63 @@
+package dev.arunkumar.scabbard.plugin
+
+import com.google.common.truth.Truth.assertThat
+import dagger.Component
+import dagger.Subcomponent
+import guru.nidi.graphviz.model.MutableGraph
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import javax.inject.Inject
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@RunWith(JUnit4::class)
+class SubcomponentCreatorBindingEdgeTest {
+
+  class NodeA @Inject constructor()
+  @Singleton
+  class NodeB @Inject constructor(private val nodeA: NodeA)
+
+  @Singleton
+  @Component
+  interface SimpleComponent {
+    fun nodeB(): NodeB
+    fun subComponentFactory(): SimpleSubComponent.Factory
+  }
+
+  @Qualifier
+  annotation class SubScope
+
+  @SubScope
+  class SubComponentNode
+  @Inject
+  constructor(val nodeB: NodeB)
+
+  @SubScope
+  @Subcomponent
+  interface SimpleSubComponent {
+    fun subcomponentNode(): SubComponentNode
+    @Subcomponent.Factory
+    interface Factory {
+      fun create(): SimpleSubComponent
+    }
+  }
+
+  private lateinit var generatedGraph: MutableGraph
+  private lateinit var generatedText: String
+
+
+  @Before
+  fun setup() {
+    generatedGraph = SimpleComponent::class.java.parsedGraph()
+    generatedText = SimpleComponent::class.java.generatedDotFile().readText()
+  }
+
+  @Test
+  fun `assert edge between component is rendered with dashed lines and label`() {
+    assertThat(generatedText).contains(" subgraph \"cluster_Subcomponents\" {")
+    assertThat(generatedText).contains("[style=\"dashed\", xlabel=\"subcomponent\"]")
+    assertThat(generatedText).contains("[label=\"dev.arunkumar.scabbard.plugin.SubcomponentCreatorBindingEdgeTest.NodeA\", color=\"turquoise\"]")
+  }
+}
