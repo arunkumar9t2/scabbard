a83c78e1e63e2b086713ade2c19c9888b7d8caa8
Arunkumar <arunk.beece@gmail.com>
2020-02-03 22:33:16 +0800

    Remove multibinding special case handling since it is already conveyed in the graph

--- scabbard-processor/src/main/java/dev/arunkumar/scabbard/plugin/parser/NodeExt.kt
+++ scabbard-processor/src/main/java/dev/arunkumar/scabbard/plugin/parser/NodeExt.kt
@@ -3,11 +3,9 @@ package dev.arunkumar.scabbard.plugin.parser
 import dagger.model.Binding
 import dagger.model.BindingGraph.ComponentNode
 import dagger.model.BindingGraph.Node
-import dagger.model.BindingKind.*
+import dagger.model.BindingKind.SUBCOMPONENT_CREATOR
 
 private const val newLine = "\\n"
-
-private data class MultiBindingData(val isMultiBinding: Boolean, val type: String)
 
 /**
  * Calculates a node's name.
@@ -25,25 +23,11 @@ internal fun Node.calculateLabel(typeNameExtractor: TypeNameExtractor): String =
       val scopeName = scopeName()
       val isSubComponentCreator = kind() == SUBCOMPONENT_CREATOR
 
-      val multiBindingData = MultiBindingData(
-        isMultiBinding = kind().isMultibinding,
-        type = when (kind()) {
-          MULTIBOUND_MAP -> "MAP"
-          MULTIBOUND_SET -> "SET"
-          else -> "UNKNOWN"
-        }
-      )
-
       key().multibindingContributionIdentifier().ifPresent { identifier ->
         name = identifier.let { it.module().split(".").last() + "." + it.bindingElement() }
       }
 
-      buildLabel(
-        name,
-        scopeName,
-        isSubComponentCreator,
-        multiBindingData
-      )
+      buildLabel(name, scopeName, isSubComponentCreator)
     } catch (e: Exception) {
       e.printStackTrace()
       "Errored $this"
@@ -60,8 +44,7 @@ internal fun Node.calculateLabel(typeNameExtractor: TypeNameExtractor): String =
 private fun buildLabel(
   name: String,
   scopeName: String? = null,
-  isSubComponentCreator: Boolean = false,
-  multiBindingData: MultiBindingData? = null
+  isSubComponentCreator: Boolean = false
 ) = buildString {
   scopeName?.let {
     append(scopeName)
@@ -71,9 +54,5 @@ private fun buildLabel(
   if (isSubComponentCreator) {
     append(newLine)
     append("Subcomponent Creator")
-  }
-  if (multiBindingData?.isMultiBinding == true) {
-    append(newLine)
-    append(multiBindingData.type)
   }
 }
