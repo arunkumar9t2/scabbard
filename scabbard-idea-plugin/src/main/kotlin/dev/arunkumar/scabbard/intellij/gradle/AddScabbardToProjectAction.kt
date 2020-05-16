755240b20afa6238f2bc80e2f1e7201c1e4a2f19
Arunkumar <arunk.beece@gmail.com>
2020-05-16 14:52:43 +0800

    Add template for Add Scabbard to project action

---
+++ scabbard-idea-plugin/src/main/kotlin/dev/arunkumar/scabbard/intellij/gradle/AddScabbardToProjectAction.kt
@@ -0,0 +1,9 @@
+package dev.arunkumar.scabbard.intellij.gradle
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class AddScabbardToProjectAction : AnAction() {
+  override fun actionPerformed(e: AnActionEvent) {
+  }
+}
