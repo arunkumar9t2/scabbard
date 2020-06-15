8a89ca7c9dbbb408b55d4100831c45da14657534
Arunkumar <arunk.beece@gmail.com>
2020-06-16 00:04:50 +0800

    Initial work on Hilt Sample

---
+++ samples/android-kotlin-hilt/src/main/java/dev/arunkumar/scabbard/sample/hilt/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.arunkumar.scabbard.sample.hilt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
