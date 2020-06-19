de08da095b6d9ee71a3b66cfbd051720331223ff
Arunkumar <arunk.beece@gmail.com>
2020-06-19 21:33:21 +0800

    Add Hilt custom components sample

---
+++ samples/android-kotlin-hilt/src/main/java/dev/arunkumar/scabbard/sample/hilt/HiltCustomComponents.kt
@@ -0,0 +1,40 @@
+package dev.arunkumar.scabbard.sample.hilt
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.DefineComponent
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Inject
+import javax.inject.Named
+
+@DefineComponent(parent = ApplicationComponent::class)
+interface HiltCustomComponent {
+  @DefineComponent.Builder
+  interface Builder {
+    fun build(): HiltCustomComponent
+  }
+}
+
+private const val NUMBER = "number"
+
+@Module
+@InstallIn(HiltCustomComponent::class)
+object HiltCustomModule {
+  @Provides
+  @Named(NUMBER)
+  fun providesNumber(): Int = 10
+
+}
+
+class CustomBinding @Inject constructor(@param:Named(NUMBER) private val number: Int)
+
+/**
+ * Entry point for exposing `HiltCustomComponent`'s bindings
+ */
+@EntryPoint
+@InstallIn(HiltCustomComponent::class)
+interface HiltCustomEntryPoint {
+  fun customBinding(): CustomBinding
+}
--- samples/android-kotlin-hilt/src/main/java/dev/arunkumar/scabbard/sample/hilt/HiltSampleApp.kt
+++ samples/android-kotlin-hilt/src/main/java/dev/arunkumar/scabbard/sample/hilt/HiltSampleApp.kt
@@ -2,6 +2,10 @@ package dev.arunkumar.scabbard.sample.hilt
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class HiltSampleApp : Application()
+class HiltSampleApp : Application() {
+  @Inject
+  lateinit var hiltCustomComponentBuilder: HiltCustomComponent.Builder
+}
