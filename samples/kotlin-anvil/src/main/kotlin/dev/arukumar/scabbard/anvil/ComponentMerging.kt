a779689791878564294a80ad499c5dbb0ffda991
Arunkumar <arunk.beece@gmail.com>
2021-04-16 01:59:16 +0800

    Setup sample module with Square's Anvil

---
+++ samples/kotlin-anvil/src/main/kotlin/dev/arukumar/scabbard/anvil/ComponentMerging.kt
@@ -0,0 +1,34 @@
+package dev.arukumar.scabbard.anvil
+
+import com.squareup.anvil.annotations.ContributesTo
+import com.squareup.anvil.annotations.MergeComponent
+import dagger.Binds
+import dagger.Component
+import dagger.Module
+import javax.inject.Inject
+
+interface AppScope
+
+interface Binding
+
+class DefaultBinding @Inject constructor() : Binding
+
+@Module
+@ContributesTo(AppScope::class)
+interface DaggerModule {
+  @Binds
+  fun DefaultBinding.bind(): Binding
+}
+
+@ContributesTo(AppScope::class)
+interface ComponentInterface {
+  fun binding(): Binding
+}
+
+@MergeComponent(AppScope::class)
+interface AppComponent {
+  @Component.Factory
+  interface Factory {
+    fun create(): AppComponent
+  }
+}
